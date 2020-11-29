c607a426309d0e928041b30e43a0e8afd444a793
DaMalle <marcfriis@protonmail.com>
2020-11-29 03:36:05 -0800

    created a base setup

---
+++ booking/data/database.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+# built-in imports
+import sqlite3
+
+class Database:
+    def __init__(self, name):
+        self._connection = sqlite3.connect(name)
+        self._cursor = self._connection.cursor()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    @property
+    def connection(self):
+        return self._connection
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def commit(self):
+        self.connection.commit()
+
+    def close(self, commit=True):
+        if commit:
+            self.commit()
+        self.connection.close()
+
+    def execute(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def query(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
+        return self.fetchall()
---
+++ booking/logic/test.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+from booking.data.tefi import myfunc
+
+myfunc
