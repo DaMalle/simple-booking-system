ac750ef38eef17d0a853c2acbb345a33ff926e34
Geileren <jakob.geil@gmail.com>
2020-11-30 09:13:24 +0100

    rettet variabelnavne i login_page

--- booking/layout/login_page.py
+++ booking/layout/login_page.py
@@ -6,21 +6,21 @@ import booking.logic.user_logic as user_logic
 
 class LoginPage(tk.Frame):
     """A frame where you can login to the booking system"""
-    def __init__(self, main=None):
+    def __init__(self, main):
         super().__init__(main)
         self.main = main
         self.draw_widgets()
 
-    def draw_widgets(self): #! change variable names
-        self.user_entry = tk.Entry(self)
-        self.user_entry.grid(column=1, row=1, pady=(50,0))
-        self.user_label = tk.Label(self, text="Username")
-        self.user_label.grid(column=0, row=1, padx=(50,0), pady=(50,0))
+    def draw_widgets(self):
+        self.username_entry = tk.Entry(self)
+        self.username_entry.grid(column=1, row=1, pady=(50,0))
+        self.username_label = tk.Label(self, text="Username")
+        self.username_label.grid(column=0, row=1, padx=(50,0), pady=(50,0))
 
-        self.pass_entry = tk.Entry(self)
-        self.pass_entry.grid(column=1, row=2, pady=(0,50))
-        self.pass_label = tk.Label(self, text="Password")
-        self.pass_label.grid(column=0, row=2, padx=(50,0), pady=(0,50))
+        self.password_entry = tk.Entry(self)
+        self.password_entry.grid(column=1, row=2, pady=(0,50))
+        self.password_label = tk.Label(self, text="Password")
+        self.password_label.grid(column=0, row=2, padx=(50,0), pady=(0,50))
 
         self.login_button = tk.Button(self,text="Login", command=self.login)
         self.login_button.grid(column=2, row=2, padx=(0,50), pady=(0,50))
--- booking/layout/test/layouttest.py
+++ booking/layout/test/layouttest.py
@@ -3,6 +3,8 @@ import tkinter as tk
 
 if __name__ == '__main__':
     root = tk.Tk()
-    LoginPage(main=root).grid(column=1,row=1)
+    LoginPage(main=root).grid(column=1, row=1)
+    header = tk.Label(root, text="   Aarhus Golfclub    ", bg="#af2222", fg="#ffffff", font=("Roboto Medium", 30, "bold"))
+    header.grid(column=1, row=0, columnspan=3)
     root.mainloop()
 
