d1711e555a34ae1e5fd4c1c627eff89cc3a7562e
Marcus <58878411+DaMalle@users.noreply.github.com>
2020-12-04 12:31:58 +0100

    Update user_logic.py

--- booking/logic/user_logic.py
+++ booking/logic/user_logic.py
@@ -1,6 +1,7 @@
+#! /usr/bin/env python3
+
 def verify_user(username, password): #verify if a user exsists in the system
     return "user verified"
-
 
 def register_user(username, password): #save a new user
     return "new user saved"
