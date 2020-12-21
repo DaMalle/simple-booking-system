168d108ec95bfebfa2f0029238abf82437189753
Thien <thienngu2002@gmail.com>
2020-12-21 21:05:48 +0100

    Added unittest

---
+++ booking/layout/test/testing.py
@@ -0,0 +1,17 @@
+import unittest 
+
+class db_test(unittest.TestCase):
+
+    def test_email(self):
+        pass
+
+    def test_password(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
