c71cfbf6e560ee711c35b2acefd5a2c9307348a1
DaMalle <marcfriis@protonmail.com>
2020-12-21 13:18:57 -0800

    added reservations

---
+++ booking/data/reservation_data.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+# Local imports
+from booking.data.database import Database
+
+class ReservationData:
+    def __init__(self):
+        with Database() as db:
+            db.execute('''CREATE TABLE IF NOT EXISTS Reservation (
+                            time text Not NULL,
+                            email text NOT NULL,
+                            first_name text NOT NULL,
+                            last_name text NOT NULL
+                           ''')
+
+    def add(self, time, email, first_name, last_name):
+        with Database() as db:
+            db.execute('INSERT INTO Reservation (time, email, first_name, last_name) VALUES (?,?,?,?)', (time, email, first_name, last_name))
