7ab1632495530204de9629114929367bc7e8cba7
Geileren <jakob.geil@gmail.com>
2020-12-22 09:52:40 +0100

    updated table

--- booking/layout/components/table.py
+++ booking/layout/components/table.py
@@ -1,4 +1,9 @@
+# built-in imports
 import tkinter as tk
+# local imports
+from booking.logic.reservation_logic import ReservationLogic
+
+
 class Table_field:
 
     def __init__(self, parent, x, y, text):
@@ -6,9 +11,9 @@ class Table_field:
         self.text = text
         self.x = x
         self.y = y
-        self.time = f"{self.x+9}:{self.text}"
+        self.time = f"{self.x + 9}:{self.text}"
         self.button = tk.Button(self.parent, text=self.text, width=3, bg="white", command=self.but_pos)
-        self.button.grid(column=self.x, row=self.y+2)
+        self.button.grid(column=self.x, row=self.y + 2)
 
     def but_pos(self):
         if self.parent.reserve_status == True:
@@ -18,29 +23,32 @@ class Table_field:
             self.parent.new_reservations.append(self)
 
 
-
-
 class Table(tk.Frame):
     """A frame in which you can view bookings in the booking system"""
+
     def __init__(self, main, user):
         super().__init__(main)
         self.main = main
         self.user = user
         self.time_list = ["00", "07", "15", "22", "30", "37", "45", "52"]
         self.reserve_status = False
-
+        self.logic = ReservationLogic()
+        self.fields = []
         self.new_reservations = []
-        self.draw_widgets()
 
+        self.draw_widgets()
 
     def draw_widgets(self):
         hour = 9
         for i in range(10):
-            tk.Button(self, text=str(hour), state="disabled", width=3, height=2,  bg="#72DBFF").grid(column=i, row=1)
+            tk.Button(self, text=str(hour), state="disabled", width=3, height=2, bg="#72DBFF").grid(column=i, row=1)
             hour += 1
         for x in range(10):
             for y in range(8):
-                Table_field(self, x, y, text=self.time_list[y])
+                self.fields.append(Table_field(self, x, y, text=self.time_list[y]))
+
+        print(self.fields)
+
         self.reserve_button = tk.Button(self, text="Reserver tider", command=self.reserve)
         self.reserve_button.grid(column=12, row=8)
 
@@ -58,18 +66,17 @@ class Table(tk.Frame):
             self.reserve_button.config(relief="raised")
             self.reserve_status = False
 
-
     def accept(self):
         for i in self.new_reservations:
-            self.pseudo_reserve(i.time, self.user)
+            self.logic.reservate(i.time, self.user)
             i.button.config(state="disabled", bg="green", fg="red")
         self.new_reservations = []
-
 
     def cancel(self):
         for i in self.new_reservations:
             i.button.config(bg="white")
         self.new_reservations = []
 
-    def pseudo_reserve(self, time, user):
-        pass
+
+
+
